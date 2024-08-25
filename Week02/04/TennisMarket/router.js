cc4670d80ae0ead82f67f8ac307a35831aff4e4a
7evendays <codingkirby0@gmail.com>
2024-08-25 21:13:53 +0900

    [practice] Week02/04 - Server

--- Week02/04/TennisMarket/router.js
+++ Week02/04/TennisMarket/router.js
@@ -3,7 +3,10 @@ function route(pathname, handle, response) { // route 함수 정의: pathname과
         console.log("pathname: " + pathname); // request.url 출력, console.log란 터미널에 출력하는 함수
         handle[pathname](response); // handle 객체의 pathname 키에 해당하는 함수 호출
     } else { // handle 객체에 pathname 키가 존재하지 않을 경우
-        return; // 아무 것도 하지 않음
+        console.log("No request handler found for " + pathname); // request.url 출력
+        response.writeHead(404, {"Content-Type": "text/html"}); // 응답 헤더 작성: 404는 Not Found를 의미, Content-Type은 text/html로 설정
+        response.write("404 Not found"); // 응답 본문 작성: body에 404 Not found를 출력
+        response.end(); // 응답 종료: 클라이언트에게 응답을 전송하고 응답을 종료
     }
 }
 
