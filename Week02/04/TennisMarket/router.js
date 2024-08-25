abaa54bf7f9e70d903a433fe7f65d37547c3a9a5
7evendays <68999618+CodingKirby@users.noreply.github.com>
2024-08-25 20:05:43 +0900

    [practice] Week02/04 - Server

--- Week02/04/TennisMarket/router.js
+++ Week02/04/TennisMarket/router.js
@@ -1,7 +1,10 @@
-function route(pathname, handle) { // route 함수 정의: pathname과 handle을 인자로 받음
-    console.log("pathname: " + pathname); // request.url 출력, console.log란 터미널에 출력하는 함수
-
-    handle[pathname]; // handle 객체의 pathname 프로퍼티에 해당하는 함수 호출
+function route(pathname, handle, response) { // route 함수 정의: pathname과 handle을 인자로 받음
+    if (typeof handle[pathname] === 'function') { // handle 객체에 pathname 키가 존재하고, 그 값이 함수일 경우
+        console.log("pathname: " + pathname); // request.url 출력, console.log란 터미널에 출력하는 함수
+        handle[pathname](response); // handle 객체의 pathname 키에 해당하는 함수 호출
+    } else { // handle 객체에 pathname 키가 존재하지 않을 경우
+        return; // 아무 것도 하지 않음
+    }
 }
 
 exports.route = route; // route 함수를 외부에서 사용할 수 있도록 exports 객체에 저장
--- Week02/04/TennisMarket/server.js
+++ Week02/04/TennisMarket/server.js
@@ -1,15 +1,10 @@
 let http = require('http'); // http 프로토콜을 사용하기 위한 모듈 *require: Node.js에서 제공하는 함수로, http 모듈을 불러옴
-let url = require('url'); // url을 파싱하기 위한 모듈
-const { route } = require('./router');
+let url = require('url'); // url 모듈을 사용하기 위한 모듈 *require: Node.js에서 제공하는 함수로, url 모듈을 불러옴
 
 function start(route, handle) { // start 함수 정의: route와 handle을 인자로 받음
     function onRequest(request, response) { // onRequest 함수 정의
         let pathname = url.parse(request.url).pathname; // url 모듈의 parse 함수를 호출하여 request.url을 파싱
-        route(pathname, handle); // route 함수 호출
-
-        response.writeHead(200, {"Content-Type": "text/html"}); // 응답 헤더 작성: 200은 성공을 의미, Content-Type은 text/html로 설정
-        response.write("Hello Node.js"); // 응답 본문 작성: body에 Hello Node.js를 출력
-        response.end(); // 응답 종료: 클라이언트에게 응답을 전송하고 응답을 종료
+        route(pathname, handle, response); // route 함수 호출
     }
     
     // http 모듈의 createServer 함수를 호출하여 서버를 생성
