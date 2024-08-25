1b5772087de877a343b89d114bc5fcc60b546f46
7evendays <codingkirby0@gmail.com>
2024-08-26 02:29:00 +0900

    [practice] Week02/06

--- Week02/04/TennisMarket/index.js
+++ Week02/04/TennisMarket/index.js
@@ -2,4 +2,7 @@ let server = require("./server"); // server.js 파일을 불러와 server 변수
 let router = require("./router"); // router.js 파일을 불러와 router 변수에 저장
 let requestHandler = require("./requestHandler"); // requestHandlers.js 파일을 불러와 requestHandlers 변수에 저장
 
+const mariadb = require('./database/connect/mariadb'); // mariadb.js 파일을 불러와 mariadb 변수에 저장
+mariadb.connect(); // mariadb 연결
+
 server.start(router.route, requestHandler.handle); // server.js 파일의 start 함수 호출
--- Week02/04/TennisMarket/requestHandler.js
+++ Week02/04/TennisMarket/requestHandler.js
@@ -1,5 +1,12 @@
+const mariadb = require('./database/connect/mariadb'); // mariadb 모듈을 불러옴
+
 function main(response) { // main 함수 정의: '/' 경로로 요청이 들어왔을 때 실행될 함수
   console.log('Request handler "main" was called.');
+
+  // mariadb 모듈의 query 함수 호출: product 테이블의 모든 데이터를 가져옴
+  mariadb.query('SELECT * FROM product', function (err, rows) { // 쿼리문과 콜백 함수를 인자로 받음
+    console.log(rows); // product 테이블의 모든 데이터 출력
+  })
 
   response.writeHead(200, {"Content-Type": "text/html"}); // 응답 헤더 작성: 200은 성공을 의미, Content-Type은 text/html로 설정
   response.write("Main page"); // 응답 본문 작성: body에 Hello Node.js를 출력
