be24dd814ec560afb94e8951f68617c075514ad6
7evendays <68999618+CodingKirby@users.noreply.github.com>
2024-08-26 22:11:24 +0900

    [practice] Week02/04/WebStart

--- Week02/04/WebStart/server.js
+++ Week02/04/WebStart/server.js
@@ -4,9 +4,7 @@ let url = require('url'); // url 모듈을 사용하기 위한 모듈 *require: 
 function start(route, handle) { // start 함수 정의: route와 handle을 인자로 받음
     function onRequest(request, response) { // onRequest 함수 정의
         let pathname = url.parse(request.url).pathname; // url 모듈의 parse 함수를 호출하여 request.url을 파싱
-        let queryData = url.parse(request.url, true).query; // url 모듈의 parse 함수를 호출하여 request.url을 파싱
-
-        route(pathname, handle, response, queryData.productId); // route 함수 호출
+        route(pathname, handle, response); // route 함수 호출: pathname, handle, response를 인자로 전달
     }
     
     // http 모듈의 createServer 함수를 호출하여 서버를 생성
