2012deafdb9ff8486215020be4b95ba0699c0a2a
7evendays <codingkirby0@gmail.com>
2024-09-03 23:31:44 +0900

    [practice] Week03/06 - express

--- Week03/06/Node-base/express-demo.js
+++ Week03/06/Node-base/express-demo.js
@@ -5,11 +5,16 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-// API: GET + "/": "http://localhost:3000/test"
+// API: GET + "http://localhost:3000/test"
 // "TEST SUCCESS" 출력
 app.get('/test', function (req, res) {
   res.send('TEST SUCCESS')
 })
 
+// API: GET + "/test/1"
+// "One!!" 출력
+app.get('/test/1', function (req, res) {
+  res.send('One!!')
+})
 
 app.listen(3000)
