bb4c840d24607a06572c48c7cba97d00d0b42f92
7evendays <codingkirby0@gmail.com>
2024-09-12 04:23:35 +0900

    [practice] Week04/05

--- Week04/05/Node-base/demo-api/youtuber-demo.js
+++ Week04/05/Node-base/demo-api/youtuber-demo.js
@@ -27,39 +27,44 @@ const express = require('express')
 const app = express()
 app.listen(3000)
 
+// 유투버 개별 조회
 app.get('/youtubers/:id', function (req, res) {
     let {id} = req.params // const를 사용하면 id를 재할당할 수 없기 때문에 오류가 발생한다
     id = parseInt(id)
     let youtuber = db.get(id)
 
     if (youtuber === undefined) {
-        res.json({
+        res.status(404).json({
             message: "유투버 정보를 찾을 수 없습니다."
         })
-        return
     }
-
-    res.json(youtuber)
+    else {
+        res.json(youtuber)
+    }
 })
 
 app.use(express.json()) // http 외 모듈인 '미들웨어': json 설정
 app.post('/youtubers', function (req, res) {
     console.log(req.body)
+    const { channelTitle, sub=0, videoNum=0 } = req.body
 
-    let {channelTitle, sub=0, videoNum=0} = req.body
-    let youtuber = {
-        "channelTitle": channelTitle,
-        "sub": sub,
-        "videoNum": videoNum
+    if (channelTitle) {
+        // sub과 videoNum은 기본값으로 설정
+        const youtuber = { channelTitle, sub, videoNum }
+
+        // db에 추가
+        db.set(id++, youtuber)
+
+        // 응답
+        res.status(201).json({
+            message: `${db.get(id-1).channelTitle}님, 유투버 생활을 응원합니다!`
+        })
     }
-
-    // db에 추가
-    db.set(id++, youtuber)
-
-    // 응답
-    res.json({
-        message: `${db.get(id-1).channelTitle}님, 유투버 생활을 응원합니다!`
-    })
+    else {
+        res.status(400).json({
+            message: "채널명을 입력해주세요."
+        })
+    }
 })
 
 // 유투버 전체 조회
@@ -68,9 +73,6 @@ app.get('/youtubers', function (req, res) {
     // console.log(db.set()) // Map 객체의 키와 값들을 추출: Map(4) {size: 4, 1 => {channelTitle, …}, 2 => {channelTitle, …}, 3 => {…}, undefined => undefined}
     
     let youtubers = {}
-    db.forEach(function (youtuber, id) { // foEach로 Map 객체의 값들만 추출
-        youtubers[id] = youtuber
-    })
 
     // Map을 일반 객체로 변환
     // let youtubers = Object.fromEntries(db)
@@ -78,13 +80,21 @@ app.get('/youtubers', function (req, res) {
     // Map을 배열로 변환
     // let youtubers = Array.from(db)
 
-    // 응답
-    res.json(youtubers)
+    if (db.size > 0) {
+        db.forEach(function (youtuber, id) { // foEach로 Map 객체의 값들만 추출
+            youtubers[id] = youtuber
+        })
+        // 응답
+        res.json(youtubers)
+    }
+    else {
+        res.status(404).json({
+            message: "유투버 정보가 없습니다."
+        })
+    }
 })
 
-let msg = ""
-
-// 유투버 삭제
+// 유투버 개별 삭제
 app.delete('/youtubers/:id', function (req, res) {
     let {id} = req.params
     id = parseInt(id)
@@ -93,34 +103,32 @@ app.delete('/youtubers/:id', function (req, res) {
     if (youtubuer) { // youtubuer가 존재하면
         // db에서 삭제
         db.delete(id)
-        msg = `${youtuber.channelTitle}님, 유투버 정보가 삭제되었습니다.`
+        res.json({
+            message: `${youtuber.channelTitle}님, 유투버 정보가 삭제되었습니다.`
+        })
     }
     else { // youtuber가 존재하지 않으면
-        msg = `${id}번 유투버 정보를 찾을 수 없습니다.`
-    }
-    
-    // 응답
-    res.json({
-        message: msg
-    })
+        res.status(404).json({
+            message: `${id}번 유투버 정보를 찾을 수 없습니다.`
+        })
+    }    
 })
 
-// 전체 유투버 삭제
+// 유투버 전체 삭제
 app.delete('/youtubers', function (req, res) {
     // db에 값이 없으면, "삭제할 유투버 정보가 없습니다." 출력
-    if (db.size === 0) {
-        msg = "삭제할 유투버 정보가 없습니다."
-    }
-    else { // db에 값이 1개 이상이면, 전체 삭제
+    if (db.size > 0) { // db에 값이 1개 이상이면, 전체 삭제
         // db 초기화
         db.clear()
-        msg = "모든 유투버 정보가 삭제되었습니다."
+        res.json({
+            message: "모든 유투버 정보가 삭제되었습니다."
+        })
     }
-    
-    // 응답
-    res.json({
-        message: msg
-    })
+    else { 
+        res.status(404).json({
+            message: "삭제할 유투버 정보가 없습니다."
+        })
+    }
 })
 
 // 유투버 정보 수정
@@ -129,22 +137,22 @@ app.put('/youtubers/:id', function (req, res) {
     id = parseInt(id)
     let youtuber = db.get(id)
 
-    if (youtuber === undefined) {
+    if (youtuber) {
+        let oldTitle = youtuber.channelTitle
+        let newTitle = req.body.channelTitle
+        youtuber.channelTitle = newTitle
+        
+        // db 수정
+        db.set(id, youtuber)
+
+        // 응답
         res.json({
+            message: `${oldTitle}님, 채널명이 ${newTitle}로 수정되었습니다.`
+        })
+    }
+    else {
+        res.status(404).json({
             message: `${id}번 유투버 정보를 찾을 수 없습니다.`
         })
-        return
     }
-
-    let oldTitle = youtuber.channelTitle
-    let newTitle = req.body.channelTitle
-    youtuber.channelTitle = newTitle
-    
-    // db 수정
-    db.set(id, youtuber)
-
-    // 응답
-    res.json({
-        message: `${oldTitle}님, 채널명이 ${newTitle}로 수정되었습니다.`
-    })
 })
