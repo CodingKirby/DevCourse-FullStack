ceb3a83325921c3fd4b91cbee34d3f7e646ee7fc
7evendays <codingkirby0@gmail.com>
2024-10-24 14:36:34 +0900

    [Week08/04] BookShop - cartItems

--- Week08/02/Book-shop/routes/carts.js
+++ Week08/02/Book-shop/routes/carts.js
@@ -1,25 +1,15 @@
 const express = require('express');
+const { addCartItems, getCartItems, removeCartItems } = require('../../../04/Book-shop/controller/CartController');
 const router = express.Router();
 
 // 장바구니 담기
-router.post('/', (req, res) => {
-    res.send('장바구니 담기')
-});
+router.post('/', addCartItems);
 
 // 장바구니 목록 조회
-router.get('/', (req, res) => {
-    res.send('장바구니 목록 조회')
-});
+// 장바구니에서 선택한 상품 목룍 조회 (=주문 예정 상품)
+router.get('/', getCartItems);
 
 // 장바구니 상품 삭제
-router.delete('/:id', (req, res) => {
-    res.send('장바구니 상품 삭제')
-});
-
-// 장바구니에서 선택한 상품 목룍 조회 (=주문 예정 상품)
-// 아직 고안 중
-// router.get('/selected', (req, res) => {
-//     res.send('장바구니에서 선택한 상품 목록 조회')
-// });
+router.delete('/:id', removeCartItems);
 
 module.exports = router;
