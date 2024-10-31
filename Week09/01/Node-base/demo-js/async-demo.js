e7643e399679bb99ca6ba1a9917ffafd05633b9e
7evendays <codingkirby0@gmail.com>
2024-10-31 17:16:56 +0900

    [Week09/01] async-await practice

--- Week09/01/Node-base/demo-js/async-demo.js
+++ Week09/01/Node-base/demo-js/async-demo.js
@@ -5,16 +5,19 @@
 // _____ function f() {}: 일반 함수
 // async function f() {}: async 함수
 
+// async의 첫 번째 기능: Promise 객체 반환
 async function f() {
-    return '완료';
+    return '완료'; // 자동으로 Promise 객체를 반환 중
+    // = return Promise.resolve('완료');
+    // async 함수는 무조건 Promise 객체를 반환한다.
+    // 만약 반환값이 Promise 객체가 아니라면, 자동으로 Promise.resolve()로 감싸서 반환한다.
 };
 
 f().then(
     (result) => {
-        console.log(result);
+        console.log("promise resolve: ", result);
     },
     (error) => {
-        console.error(error);
+        console.error("promise reject: ", error);
     }
 );
-
