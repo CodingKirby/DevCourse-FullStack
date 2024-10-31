f5139d2811d6c9d63f9e68e4c5c027ede638834f
7evendays <codingkirby0@gmail.com>
2024-10-31 16:14:27 +0900

    [Week09/01] bookshop

---
+++ Week09/01/Book-shop/routes/likes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { addLikes, removeLikes } = require('../controller/LikeController');
+
+// 좋아요 표시 = 추가
+router.post('/:id', addLikes);
+
+// 좋아요 취소 = 삭제
+router.delete('/:id', removeLikes);
+
+module.exports = router;
