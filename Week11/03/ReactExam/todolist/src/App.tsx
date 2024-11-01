2672a425b488ed96a216eb5540ace36ac0350c80
7evendays <codingkirby0@gmail.com>
2024-11-01 17:21:00 +0900

    practice: Week11/03 - Update app.tsx

--- Week11/03/ReactExam/todolist/src/App.tsx
+++ Week11/03/ReactExam/todolist/src/App.tsx
@@ -4,26 +4,45 @@ import './App.css';
 
 // JSX를 사용한 코드
 function App() {
+  // 변수 선언
   let name = '리액트';
-  const stlye = {
-    // css 문법과는 다르게 camelCase를 사용한다. = background-color가 허용되지 않는다.
-    
-  }
+  
+  // style 객체 선언
+  const style = {
+    backgroundColor: '#61dafb',
+    color: '#282c34',
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    borderRadius: 16,
+    padding: 16
+  };
 
   return (
-    <div style={
-      {
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: '20px'
-      }
-    }>
+    <div className="container">
+      {/* 주석 작성 방법 */}
+      {/*
+        작성자: 코딩커비
+        작성일: 2024년 10월 30일
+        내용: 기능에 대한 내용 
+      */}
       <header className="hello">
         <h1>Hello, {
-        name === '리액트' ? (<h1>YES</h1>) : (<h1>NO</h1>)
+        name === '리액트' ? ('YES') : ('NO')
         }!!</h1>
+
+        {/* 인라인 스타일링: JSX에서는 style을 객체 형태로 작성 */}
+        <p style={{
+          backgroundColor: '#61dafb',
+          color: '#282c34',
+          fontSize: '2rem',
+          fontWeight: 'bold',
+          borderRadius: 16,
+          padding: 16
+        }}>안녕하세요.</p>
+        <p style={style}>{name}</p>
+
+        {/* self-closing tag */}
+        <br />
         <p>반갑습니다.</p>
       </header>
     </div>
@@ -38,6 +57,19 @@ function App() {
 //       React.createElement('h1', null, 'Hello, 리액트!'),
 //       React.createElement('p', null, '반갑습니다.')
 //     )
+//   );
+// }
+
+// undefined 처리
+// function App() {
+//   const port = undefined;
+  
+//   return (
+//     <div>
+//       {
+//         port || <h1>포트가 없습니다.</h1>
+//       }
+//     </div>
 //   );
 // }
 
