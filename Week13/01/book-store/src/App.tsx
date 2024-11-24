e014e436af80db84e49b7be09bb7e470558f2780
7evendays <codingkirby0@gmail.com>
2024-11-24 16:21:45 +0900

    Project: Week13/01/book-store - 글로벌 스타일, 테마

--- Week13/01/book-store/src/App.tsx
+++ Week13/01/book-store/src/App.tsx
@@ -1,9 +1,16 @@
 import Home from "./pages/Home";
 import Layout from "./components/layout/Layout";
+import ThemeSwitcher from "./components/header/ThemeSwitcher";
+import { BookStoreThemeProvider } from "./context/themeContext";
 
 function App() {
   return (
-    <Layout children={<Home />} />
+    <BookStoreThemeProvider>
+      <ThemeSwitcher />
+      <Layout>
+        <Home />
+      </Layout>
+    </BookStoreThemeProvider>
   );
 }
 
