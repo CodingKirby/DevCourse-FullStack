147a440619ad48c585ba8e20fda8962d9d366c7b
7evendays <codingkirby0@gmail.com>
2024-12-08 04:05:04 +0900

    fix: carts 라우터에 컨트롤러 함수 매핑

--- Week08/04/Book-shop/routes/carts.js
+++ Week08/04/Book-shop/routes/carts.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { addCartItems, getCartItems, removeCartItems } = require('../controllers/CartController');
 
 // 장바구니 담기
-router.post('/', (req, res) => {
-    res.send('장바구니 담기')
-});
+router.post('/', addCartItems);
 
 // 장바구니 목록 조회
-router.get('/', (req, res) => {
-    res.send('장바구니 목록 조회')
-});
+router.get('/', getCartItems);
 
 // 장바구니 상품 삭제
-router.delete('/:id', (req, res) => {
-    res.send('장바구니 상품 삭제')
-});
+router.delete('/:id', removeCartItems);
 
 // 장바구니에서 선택한 상품 목룍 조회 (=주문 예정 상품)
 // 아직 고안 중
--- Week09/03/Book-shop/routes/carts.js
+++ Week09/03/Book-shop/routes/carts.js
@@ -11,4 +11,10 @@ router.get('/', getCartItems);
 // 장바구니 상품 삭제
 router.delete('/:id', removeCartItems);
 
+// 장바구니에서 선택한 상품 목룍 조회 (=주문 예정 상품)
+// 아직 고안 중
+// router.get('/selected', (req, res) => {
+//     res.send('장바구니에서 선택한 상품 목록 조회')
+// });
+
 module.exports = router;
